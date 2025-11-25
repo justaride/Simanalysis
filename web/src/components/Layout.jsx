de9cb84952c86a6244130da45d6111660463c36f
justaride <boen.gabriel@gmail.com>
2025-11-25 17:21:10 +0100

    fix: Fix Layout component to render children correctly
    Layout was using Outlet but was not used as a layout route. Changed to render children prop.

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
-function Layout() {
+function Layout({ children }) {
     return (
         <div className="min-h-screen bg-gray-900 text-white flex">
             <Sidebar />
             <main className="flex-1 overflow-auto">
-                <Outlet />
+                {children}
             </main>
         </div>
     );
